8b8bec3cb62384acbad273426f33b458ab0bab9c
lvs2402 <67360778+lvs2402@users.noreply.github.com>
2020-12-29 15:46:13 +0800

    changed app js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,13 @@ function Header() {
   return (
     <header>
       <div>
-        <h1> TODO </h1>
+        <h1> My Website </h1>
       </div>
       <nav>
-        <a href="/?city=TODO"> City Name </a>
-        <a href="/?city=TODO"> City Name </a>
-        <a href="/?city=TODO"> City Name </a>
-        <a href="/?city=TODO"> City Name </a>
+        <a href="/?city=Singapore"> Singapore </a>
+        <a href="/?city=New%20York"> New York </a>
+        <a href="/?city=Tokyo"> Tokyo </a>
+        <a href="/?city=Jakarta"> Jakarta </a>
       </nav>
     </header>
   );
