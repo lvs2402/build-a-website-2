51c3ca44dd65c4740243a5a45bddb28970efedfa
lvs2402 <67360778+lvs2402@users.noreply.github.com>
2021-01-01 12:17:33 +0800

    final project

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,18 +1,102 @@
-import React, { useState, useEffect, useMemo } from "react";
-// import axios from "axios";
-// import { useHistory } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-import Header from "../components/Header";
-import WeatherImage from "../components/WeatherImage";
 import City from "../components/City";
-const weatherKey = `TODO`; // Your API Key here
 
 function Home() {
+  const [cities, setCities] = useState([
+    {
+      name: "Jakarta",
+      currentTemp: "0",
+      weatherType: "",
+      color: "bg-yellow-500",
+    },
+    {
+      name: "Bandung",
+      currentTemp: "0",
+      weatherType: "",
+      color: "bg-red-500",
+    },
+    {
+      name: "Malang",
+      currentTemp: "0",
+      weatherType: "",
+      color: "bg-blue-500",
+    },
+    {
+      name: "Taoyuan",
+      currentTemp: "0",
+      weatherType: "",
+      color: "bg-blue-500",
+    },
+    // {
+    //   name: "New York",
+    //   currentTemp: "0",
+    //   weatherType: "",
+    //   color: "bg-blue-500",
+    // },
+    // {
+    //   name: "Tokyo",
+    //   currentTemp: "0",
+    //   weatherType: "",
+    //   color: "bg-blue-500",
+    // },
+  ]);
+
+  useEffect(() => {
+    updateAllWeatherData();
+  }, []);
+
+  // Fetch the weather data for 1 city
+  async function fetchWeatherData(cityName) {
+    const res = await axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${process.env.REACT_APP_WEATHER_API}`
+      )
+      .then(function (response) {
+        // Successful request
+        const weather = response.data;
+        return weather;
+      })
+      .catch(function (error) {
+        // The best practice of coding is to not use console.log
+        console.warn(error);
+      });
+
+    return res;
+  }
+
+  // update the list data
+  async function updateAllWeatherData(params) {
+    cities.forEach(function (citiesItems, index) {
+      let weatherData = {};
+      let newCities = [...cities];
+
+      fetchWeatherData(citiesItems.name).then((res) => {
+        weatherData = res;
+
+        newCities[index].currentTemp = `${Math.round(weatherData.main.temp)}°C`;
+        newCities[index].weatherType = weatherData.weather[0].main;
+        setCities(newCities);
+      });
+    });
+  }
+
   return (
-    <div className="flex flex-col h-screen bg-green-200">
-      <City cityName="Singapore" temp="10°C" color="bg-pink-200" />
-      <City cityName="Jakarta" temp="10°C" color="bg-blue-200" />
-      <City cityName="Tokyo" temp="10°C" color="bg-red-200" />
+    // Container
+    <div className="flex flex-col h-screen bg-green-100">
+      <div className="text-4xl text-yellow-500 font-semibold text-center my-4">
+        Our Weather App
+      </div>
+
+      {cities.map((item, index) => (
+        <City
+          cityName={item.name}
+          weatherType={item.weatherType}
+          temp={item.currentTemp}
+          color={item.color}
+        />
+      ))}
     </div>
   );
 }
